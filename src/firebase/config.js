e17113612a98e32397d5b4de5839efdb7432c101
Almog Lev <almoglev3@gmail.com>
2022-03-04 19:56:09 +0200

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdDsVPX_YWXlJmxyiE98usFO2Fd1TgJ9U",
+    authDomain: "thepagodasite.firebaseapp.com",
+    projectId: "thepagodasite",
+    storageBucket: "thepagodasite.appspot.com",
+    messagingSenderId: "313049083076",
+    appId: "1:313049083076:web:bf2382979b9c4efd5f2d91"
+  }
+
+
+// Initialize firebase
+firebase.initializeApp(firebaseConfig)
+
+// Initialize services (firestore + authentication)
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// Set up timestamp function
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
