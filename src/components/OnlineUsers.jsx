10ebb078bbad936836aa28676b077be43118785c
Almog Lev <almoglev3@gmail.com>
2022-03-05 12:39:15 +0200

    Show online users

--- src/components/OnlineUsers.jsx
+++ src/components/OnlineUsers.jsx
@@ -13,6 +13,7 @@ export default function OnlineUsers() {
         {error && <div className='error'>{error}</div>}
         {documents && documents.map((user)=>(
             <div className='user-list-item' key={user.id}>
+                {user.online ? <span className='online-user'></span> : <span className='offline-user'></span>}
                 <span>{user.displayName}</span>
                 <Avatar src={user.photoURL} />
             </div>
