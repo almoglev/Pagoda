3170531f9819c450c5768914aa0830730bccbb88
Almog Lev <almoglev3@gmail.com>
2022-03-06 22:31:06 +0200

    Add comments to project with the useFirestore hook

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
 import { timestamp } from "../../firebase/config"
 import { useAuthContext } from "../../hooks/useAuthContext"
+import { useFirestore } from "../../hooks/useFirestore"
 
-export default function ProjectComments() {
+export default function ProjectComments({ project }) {
     const [newComment, setNewComment] = useState('')
     const { user } = useAuthContext()
+    const { updateDocument, response } = useFirestore('projects')
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -15,7 +17,16 @@ export default function ProjectComments() {
             createdAt: timestamp.fromDate(new Date()),
             id: Math.random()
         }
-        console.log(commentToAdd)
+        
+        const comments = {
+            comments: [...project.comments, commentToAdd]
+        }
+
+        await updateDocument(project.id, comments)
+
+        if (!response.error) {
+            setNewComment('')
+        }
     }
 
   return (
